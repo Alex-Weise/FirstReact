426430918c97880700e612bf2c37769375be0a91
Alex-Weise <Sannek65@gmail.com>
2022-05-23 22:39:59 +0300

    The clock from class

--- src/index.js
+++ src/index.js
@@ -1,21 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import {App, Tick} from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-  
-function tick() {
-  const element = (
-    <div className='title'>
-      <h1>The Clock!</h1>
-      <h2>Now {new Date().toLocaleTimeString()}</h2>
-    </div>
-  );
-  root.render(element);
-}
+root.render(
+  <React.StrictMode>
+    <App date={new Date()} />
+    <Tick />
+  </React.StrictMode>
+);
 
-setInterval(tick, 1000);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+  
+// function tick() {
+//   const element = (
+//     <div className='title'>
+//       <h1>The Clock!</h1>
+//       <h2>Now {new Date().toLocaleTimeString()}</h2>
+//     </div>
+//   );
+//   root.render(element);
+// }
+
+// setInterval(tick, 1000);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
